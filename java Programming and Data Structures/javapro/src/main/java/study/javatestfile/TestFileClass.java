59d785f85500c2cd8e9ab80d4867003ef3545883
giwthub <45228130+giwthub@users.noreply.github.com>
2019-10-01 22:09:06 +0800

    test file

---
+++ java Programming and Data Structures/javapro/src/main/java/study/javatestfile/TestFileClass.java
@@ -0,0 +1,20 @@
+package study.javatestfile;
+
+import java.io.File;
+import java.util.Date;
+
+public class TestFileClass {
+
+	public static void main(String[] args) throws Exception {
+		File file = new File("pom.xml");
+		System.out.println("Does it exists? "+file.exists());
+		System.out.println("The file has " + file.length() + " bytes.");
+		System.out.println("Can it be read? " +file.canRead());
+		System.out.println("Can it be written? " + file.canWrite());
+		System.out.println("Is it a directory? " + file.isDirectory());
+		System.out.println("Is it a file? " + file.isFile());
+		System.out.println("Is it hidden? " + file.isHidden());
+		System.out.println("Absolute path is " + file.getAbsolutePath());
+		System.out.println("Lst modified on " + new Date(file.lastModified()));
+	}
+}
