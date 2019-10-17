7b9ae1877b27f9b2db4bd890e536f7f28e529048
giwthub <45228130+giwthub@users.noreply.github.com>
2019-10-18 00:31:42 +0800

    2019.10.17

---
+++ java Programming and Data Structures/javapro/src/main/java/study/java/recursiveMethod/TowerOfHanoi.java
@@ -0,0 +1,28 @@
+package study.java.recursiveMethod;
+
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+	
+	public static void main(String[] args) throws Exception {
+		
+		System.out.print("Enter number of disks: ");
+		int n = + new Scanner(System.in).nextInt();
+		System.out.println("The moves are:");
+		moveDisks(n, 'A', 'B', 'C');
+	}
+
+	public static void moveDisks(int n,char fromTower, char toTower, char auxTower) {
+		
+		if (n == 1) {
+			System.out.println("Move disk " + n + " from " + fromTower + " to " + toTower);
+		}else {
+			moveDisks(n - 1, fromTower, auxTower, toTower);
+			System.out.println("Move disk " + n + " from " + fromTower + " to " + toTower  + "\n");
+			moveDisks(n - 1, auxTower, toTower, fromTower);
+			
+		}
+		
+		
+	}
+}
